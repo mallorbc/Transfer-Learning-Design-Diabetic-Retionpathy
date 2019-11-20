52c5af2bad44350fa84850dbe54df17f40cd9582
Blake Mallory <blakecmallory@gmail.com>
2019-11-20 03:48:06 +0000

    made model that can take two images for inputs

--- code/myModels.py
+++ code/myModels.py
@@ -1,8 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
-from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import Dropout,Input,concatenate
 import os
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import plot_model
+
+
 
 # from tensorflow.keras.applications import inception_v3
 
@@ -69,3 +72,43 @@ def transfer_learning_model_inception_v3(new_image_width, new_image_height,is_tr
 
     return model
 
+def inception_v3_multiple_inputs(image_width,image_height):
+    input_shape = (image_width,image_height,3)
+    image_input1 = Input(shape = input_shape)
+    image_input2 = Input(shape = input_shape)
+
+    #loads the inception_v3 model, removes the last layer, and sets inputs to the size needed
+    input1 = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_tensor = image_input1, pooling='avg')
+    input2 = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_tensor = image_input2, pooling='avg')
+    # input1 = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_shape=(image_width, image_height, 3))
+    # input2 = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_shape=(image_width, image_height, 3))
+    input1.trainable = True
+    input2.trainable = True
+    
+    #change the second branches layer names
+    for layer in input2.layers:
+        layer._name = layer.name + str("_2")
+        
+    layer1_1 = input1.output
+    layer2_1 = input2.output
+    # layer1_1 = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')(layer1_1)
+    # layer2_1 = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')(layer2_1)
+
+
+    flat1 = layers.Flatten()(layer1_1)
+    flat2 = layers.Flatten()(layer2_1)
+
+    concat_layer = concatenate([flat1,flat2])
+
+    # output = layers.Flatten()(concat_layer)
+    output = layers.Dense(1024,activation='relu')(concat_layer)
+    output = Dropout(0.5)(output)
+    output = layers.Dense(512,activation='relu')(output)
+    output = Dropout(0.5)(output)
+    output = layers.Dense(5,activation='softmax')(output)
+    final_model = models.Model(inputs=[image_input1, image_input2], outputs=output)
+    final_model.summary()
+    plot_model(final_model, to_file='model_plot2.png')
+    # plot_model(final_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+
+    return final_model
