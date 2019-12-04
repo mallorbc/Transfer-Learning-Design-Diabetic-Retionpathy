9a7eebc5341b4dc65803647af3ea293380525323
Blake Mallory <blakecmallory@gmail.com>
2019-12-04 21:01:01 +0000

    added efficientnet and inceptionresnet

--- code/myModels.py
+++ code/myModels.py
@@ -7,6 +7,15 @@ from tensorflow.keras.utils import plot_model
 
 import time
 
+# import efficientnet.keras as efn 
+# from keras_efficientnets import EfficientNetB5
+#import efficientnet.keras as efn 
+from efficientnet.tfkeras import EfficientNetB7 as Net
+
+
+
+
+
 
 
 # from tensorflow.keras.applications import inception_v3
@@ -53,6 +62,8 @@ def save_model(model_to_save,run_dir):
 def transfer_learning_model_inception_v3(new_image_width, new_image_height,is_trainable):
     #loads the inception_v3 model, removes the last layer, and sets inputs to the size needed
     base_model = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_shape=(new_image_width, new_image_height, 3))
+    # base_model = EfficientNetB5(weights='imagenet',include_top=False,input_shape=(new_image_width, new_image_height, 3))
+
     #sets the inception_v3 model to not update its weights
     base_model.trainable = is_trainable
     #layer to convert the features to a single n-elemnt vector per image
@@ -124,3 +135,82 @@ def inception_v3_multiple_inputs(image_width,image_height):
     # plot_model(final_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
     # quit()
     return final_model
+
+def efficientnet(new_image_width, new_image_height):
+    base_model = Net(weights='imagenet',include_top=False,input_shape=(new_image_width, new_image_height, 3))
+    base_model.trainable = True
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    model = models.Sequential([base_model,global_average_layer])
+    model.add(layers.Dense(1024,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(layers.Dense(512,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(layers.Dense(256,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(layers.Dense(128,activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(layers.Dense(5,activation='softmax'))
+    model.summary()
+    print("EfficientNetB7")
+    model.compile(optimizer=Adam(lr=0.000001),
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+    # model.compile(optimizer=Adam(lr=0.0000015),
+    #             loss='sparse_categorical_crossentropy',
+    #             metrics=['accuracy'])
+
+    return model
+
+
+def inception_v3_resnet(image_width,image_height):
+    input_shape = (image_width,image_height,3)
+    image_input1 = Input(shape = input_shape)
+    image_input2 = Input(shape = input_shape)
+
+    #loads the inception_v3 model, removes the last layer, and sets inputs to the size needed
+    input1 = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_tensor = image_input1, pooling='avg')
+    input2 = tf.keras.applications.ResNet50(weights="imagenet",include_top=False,input_tensor = image_input2, pooling='avg')
+    # input1 = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_shape=(image_width, image_height, 3))
+    # input2 = tf.keras.applications.InceptionV3(weights="imagenet",include_top=False,input_shape=(image_width, image_height, 3))
+    input1.trainable = True
+    input2.trainable = True
+    
+    #change the second branches layer names
+    for layer in input2.layers:
+        layer._name = layer.name + str("_2")
+        
+    layer1_1 = input1.output
+    layer2_1 = input2.output
+    # layer1_1 = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')(layer1_1)
+    # layer2_1 = tf.keras.layers.GlobalAveragePooling2D(name='avg_pool')(layer2_1)
+
+
+    flat1 = layers.Flatten()(layer1_1)
+    flat2 = layers.Flatten()(layer2_1)
+
+    concat_layer = concatenate([flat1,flat2])
+
+    # output = layers.Flatten()(concat_layer)
+    output = layers.Dense(1024,activation='relu')(concat_layer)
+    output = Dropout(0.5)(output)
+    output = layers.Dense(512,activation='relu')(output)
+    output = Dropout(0.5)(output)
+    # output = layers.Dense(256,activation='relu')(output)
+    # output = Dropout(0.5)(output)
+    # output = layers.Dense(128,activation='relu')(output)
+    # output = Dropout(0.5)(output)
+    # output = layers.Dense(64,activation='relu')(output)
+    # output = Dropout(0.5)(output)
+    output = layers.Dense(5,activation='softmax')(output)
+    final_model = models.Model(inputs=[image_input1, image_input2], outputs=output)
+    final_model.summary()
+    final_model.compile(optimizer=Adam(lr=0.000001),
+                loss='sparse_categorical_crossentropy',
+                metrics=['accuracy'])
+    time.sleep(2)
+    #plot_model(final_model, to_file='model_plot2.png')
+    # plot_model(final_model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    # quit()
+    return final_model
+
+
