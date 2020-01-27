93dcafbab779b84e348bd25a762ee4ee7d3aac9d
Blake Mallory <blakecmallory@gmail.com>
2020-01-27 07:25:33 +0000

    added code to get data from one classification and to create npy files

--- code/utils.py
+++ code/utils.py
@@ -102,3 +102,56 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def add_extension(list_of_files,extension_to_add):
+    new_list = []
+    for file in list_of_files:
+        new_list.append(file + extension_to_add)
+    return new_list
+
+def get_images_of_one_class(class_to_get,list_of_images,class_dict):
+    images_of_class = []
+
+    for image in list_of_images:
+        if class_dict[os.path.basename(image)]!=class_to_get:
+            continue
+        else:
+            images_of_class.append(image)
+    
+    return images_of_class
+
+def make_npy_of_class(class_to_get,list_of_images,class_dict,output_folder,csv_file=None):
+    images_of_one_class = get_images_of_one_class(class_to_get,list_of_images,class_dict)
+    images_of_one_class = add_extension(images_of_one_class,".jpeg")
+    images_to_use = []
+
+    if csv_file is not None:
+        class_name,images_from_csv = preprocessData.load_data(csv_file)
+        for i in range(len(images_of_one_class)):
+            for j in range(len(images_from_csv)):
+                # print(images_from_csv[j])
+                # print(images_of_one_class[i])
+                # quit()
+                if os.path.basename(images_from_csv[j]) == os.path.basename(images_of_one_class[i]):
+                    images_to_use.append(images_of_one_class[i])
+                    break
+    else:
+        images_to_use = images_of_one_class
+    
+    print(len(images_to_use))
+    # quit()
+
+    save_location_base = os.path.realpath(output_folder)
+    counter = 0
+    for image in images_to_use:
+            image_name = image
+            image = Image.open(image)
+            pixels=np.asarray(image)
+            pixels = pixels.astype('float32')
+            pixels /=255.0
+            print('Min: %.3f, Max: %.3f' % (pixels.min(), pixels.max()))
+            save_location = save_location_base + "/" + os.path.basename(image_name)
+            np.save(save_location,pixels)
+            counter = counter + 1
+            print("Made "+str(counter)+" npy files")
+
